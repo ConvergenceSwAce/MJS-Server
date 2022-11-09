d17f816682fe68b90220693396f64484e9c702b9
k-kbk <kkbk-@naver.com>
2022-11-09 13:04:48 +0900

    Feat: 식단 입력 유효성 검사 추가

--- routes/menu.ts
+++ routes/menu.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import { check, validationResult } from 'express-validator';
 import { Menu } from '../interfaces/menu';
 import Menu0 from '../models/menu0';
 import Menu10 from '../models/menu10';
@@ -8,31 +9,35 @@ import Menu12 from '../models/menu12';
 const router = Router();
 
 // 식단 입력
-router.post('/', (req: Request, res: Response) => {
-  const menu: Menu = {
-    date: req.body.date,
-    day: req.body.day,
-    cafeteria: req.body.cafeteria,
-    lunchA: req.body.lunchA,
-    lunchB: req.body.lunchB,
-    lunch: req.body.lunch,
-    dinner: req.body.dinner,
-  };
-  switch (req.body.cafeteria) {
-    case 0:
-      Menu0.create(menu);
-      break;
-    case 10:
-      Menu10.create(menu);
-      break;
-    case 11:
-      Menu11.create(menu);
-      break;
-    case 12:
-      Menu12.create(menu);
-      break;
-  }
-  res.send('ok');
-});
+router.post(
+  '/',
+  [
+    check('date').notEmpty().toDate(),
+    check('day').notEmpty(),
+    check('cafeteria').notEmpty().toInt(),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(500).json({ errors: errors.array() });
+    }
+    const menu: Menu = req.body;
+    switch (req.body.cafeteria) {
+      case 0:
+        Menu0.create(menu);
+        break;
+      case 10:
+        Menu10.create(menu);
+        break;
+      case 11:
+        Menu11.create(menu);
+        break;
+      case 12:
+        Menu12.create(menu);
+        break;
+    }
+    res.send('ok');
+  },
+);
 
 export default router;
